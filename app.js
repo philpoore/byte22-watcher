94f3c7b1ae0157691b6ac93b38fba28287e99544
Sam Hynds <samhynds@gmail.com>
2014-11-06 16:29:15 +0000

    Added keypresses, saving file on ctrl-s, showing collected data in table, help command.'

--- app.js
+++ app.js
@@ -2,16 +2,108 @@ var argv =  require('optimist')
 			.usage('Usage: $0 -c [column num] -d [delimeter] -f [filename]')
 			.argv;
 
+var fs = require('fs');
+var keypress = require('keypress')
+var tty = require('tty');
+var Table = require('cli-table');
+var readline = require('readline');
+var ft = require('file-tail');
+
 // Command Line Arguments
 var colNum = argv.c || null;
 var delimeter = argv.d || ' ';
 var file = argv.f || null;
-var fs = require('fs');
-var savedKnownFile = './knownItems.json';
+var savedKnownFile = './savedData.json';
+
+// Prevents 0 becoming null if you want to select the first column. Count from one instead of zero now.
+colNum = colNum - 1;
+
 var	knownColumns = {};
 
+
+// Help Text
+if(argv.help || argv.h) {
+	var help = "\n======================================================================\n" +
+			"File Watcher is an application that monitors a 'tabular'\n" +
+			"text file and counts how many times a string is\n" +
+			"mentioned within a column in total in the table.\n\n" +
+			"Usage (watch file):\n" +
+			"\t./watcher -c [column num] -d [delimeter] -f [filename]\n" +
+			"Usage (watch stdin):\n" +
+			"\ttail -F access.log | ./watcher -c [column num] -d [delimeter]\n\n" +
+			"Ctrl-T: Shows a tabular summary of collected data.\n" +
+			"Ctrl-S: Saves the data in a JSON object to a file.\n" +
+			"======================================================================\n";
+
+	console.log(help);
+	process.exit();
+}
+
+keypress(process.stdin);
+
+// Keypresses
+process.stdin.on('keypress', function (ch, key) {
+
+	if (key && key.ctrl && key.name == 'c') {
+		process.exit();
+	}
+
+	if (key && key.ctrl && key.name == 't') {
+
+		console.log('==========================================================\nShowing Summary of Collected Data:\n');
+
+		var table = new Table({
+		    head: ['Column Name', 'Count']
+		  , colWidths: [50, 15]
+		});
+
+		// Convert data to table-friendly format.
+		var kca = JSON.stringify(knownColumns).split(',').map(function(str){
+			str = str.replace('{','');
+			str = str.replace('}','');
+			str = str.replace('/\"/g','');
+			str = str.split(':');
+			str[1] = parseInt(str[1]);
+			return str;
+		}).sort(function(a, b) {
+
+			    var valueA, valueB;
+
+			    valueA = a[1]; 
+			    valueB = b[1];
+			    if (valueA < valueB) {
+			        return -1;
+			    }
+			    else if (valueA > valueB) {
+			        return 1;
+			    }
+			    return 0;
+
+		}).reverse().map(function(arrItem){
+			table.push(arrItem);
+			return arrItem;
+		});
+
+		console.log(table.toString());
+	}
+
+	if (key && key.ctrl && key.name == 's') {
+		console.log('\nSaving to ', savedKnownFile);
+		fs.writeFileSync(savedKnownFile, JSON.stringify(knownColumns));
+	}
+
+});
+
+if (typeof process.stdin.setRawMode == 'function') {
+  process.stdin.setRawMode(true);
+} else {
+  tty.setRawMode(true);
+}
+process.stdin.resume();
+
+
 function readFile() {
-	var ft = require('file-tail').startTailing(file);
+	ft = ft.startTailing(file);
 	console.log('Reading from file: ', file);
 
 	// Monitor 'tail' of file
@@ -26,18 +118,16 @@ function readFile() {
 		if(column in knownColumns) {
 			knownColumns[column]++;
 		} else {
-			console.log(column, 'not known.. adding to object.');
+			console.log('New!! ', column);
 			knownColumns[column] = 1;
 		}
 
-		console.log(knownColumns);
 	});
 }
 
 function readstdin() {
 	console.log('Reading from STDIN..');
 
-	var readline = require('readline');
 	var rl = readline.createInterface({
 	  input: process.stdin,
 	  output: process.stdout
@@ -53,14 +143,12 @@ function readstdin() {
 		if(column in knownColumns) {
 			knownColumns[column]++;
 		} else {
-			console.log(column, 'not known.. adding to object.');
+			console.log('New! ', column);
 			knownColumns[column] = 1;
 		}
 
 		console.log(knownColumns);
 	});
-
-	console.log('\n\n', knownColumns, '\n\n');
 }
 
 if(argv.f) {
@@ -68,9 +156,3 @@ if(argv.f) {
 } else {
 	readstdin();
 }
-
-process.on( 'SIGINT', function() {
-	console.log('\nSaving to ', savedKnownFile);
-	fs.writeFileSync(savedKnownFile, JSON.stringify(knownColumns));
-	process.exit( );
-})
--- readme.md
+++ readme.md
@@ -2,14 +2,21 @@
 
 Small application that monitors the tail of a file specified, or through stdin and allows you to monitor and count occurrences of a column on each line. For example, counting the number of referrers in a website access log.
 
+Displays most common results in a tabular format and can save data in a JSON file.
+
 ###Usage
 
 ```Shell
 ./watcher -c [colNum] -d [delimeter] -f [filename]
 ```
 
-+ colNum is the number of delimeters to skip past (counts from 0)
-	For example using -c 2 on a line like "Col1 Col2 Col3 Col4" would return just "Col3" for each line.
+####Keyboard Commands
+* Ctrl+T - Shows a table of the results in descending order.
+* Ctrl+S - Saves the data to savedData.json in the current folder.
+
+####Parameters
++ colNum is the number of delimeters to skip past (counts from 1)
+	For example using -c 2 on a line like "Col1 Col2 Col3 Col4" would return just "Col2" for each line.
 + delimeter is what each column is seperated by, default is a single space character - ' '.
 + filename is the path of the file, if -f is not specified, File Watcher will use stdin.
 
@@ -17,14 +24,15 @@ Small application that monitors the tail of a file specified, or through stdin a
 
 The following command would extract the third column from each line of a file called 'access.log':
 ```Shell
-	./watcher -c 2 -f access.log
+	./watcher -c 3 -f access.log
 ```
+
 The following command would extract the tenth column from stdin:
 ```Shell
-	cat access.log | ./watcher -c 9
+	cat access.log | ./watcher -c 10
 ```
 
 This command uses tail to follow the end of the file and pipe in to File Watcher to find the 16th column
 ```Shell
-	tail -F access.log | node . -c 15
+	tail -F access.log | ./watcher -c 16
 ```
